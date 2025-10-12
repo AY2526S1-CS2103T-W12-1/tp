01b3fc57542eab20e5d56b4bce537347312b8171
nob-s <jqlee999@gmail.com>
2025-10-12 12:17:44 +0800

    fix all checkstyle tests

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -51,7 +51,8 @@ public class PersonUtil {
         descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
         descriptor.getEmail().ifPresent(email -> sb.append(PREFIX_EMAIL).append(email.value).append(" "));
         descriptor.getAddress().ifPresent(address -> sb.append(PREFIX_ADDRESS).append(address.value).append(" "));
-        descriptor.getActivities().ifPresent(activities -> sb.append(PREFIX_ACTIVITIES).append(activities.activities).append(" "));
+        descriptor.getActivities().ifPresent(
+                activities -> sb.append(PREFIX_ACTIVITIES).append(activities.activities).append(" "));
         if (descriptor.getTags().isPresent()) {
             Set<Tag> tags = descriptor.getTags().get();
             if (tags.isEmpty()) {
