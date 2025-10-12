9e99f0c54044305fdd29af44878c504d2306876d
Wang Boqian <e1408896@u.nus.edu>
2025-10-12 15:02:27 +0800

    Rename and modify Phone class to Priority class

--- src/main/java/seedu/address/model/person/Priority.java
+++ src/main/java/seedu/address/model/person/Priority.java
@@ -4,32 +4,32 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Person's phone number in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidPhone(String)}
+ * Represents an attractions priority number in Maplet.
+ * Guarantees: immutable; is valid as declared in {@link #isValidPriority(String)}
  */
-public class Phone {
+public class Priority {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone numbers should only contain numbers, and it should be at least 3 digits long";
-    public static final String VALIDATION_REGEX = "\\d{3,}";
+            "Priority level should be from a number from 1 - 10";
+    public static final String VALIDATION_REGEX = "\\b([1-9]|10)\\b";
     public final String value;
 
     /**
-     * Constructs a {@code Phone}.
+     * Constructs a {@code Priority}.
      *
-     * @param phone A valid phone number.
+     * @param priority A valid priority value.
      */
-    public Phone(String phone) {
-        requireNonNull(phone);
-        checkArgument(isValidPhone(phone), MESSAGE_CONSTRAINTS);
-        value = phone;
+    public Priority(String priority) {
+        requireNonNull(priority);
+        checkArgument(isValidPriority(priority), MESSAGE_CONSTRAINTS);
+        value = priority;
     }
 
     /**
      * Returns true if a given string is a valid phone number.
      */
-    public static boolean isValidPhone(String test) {
+    public static boolean isValidPriority(String test) {
         return test.matches(VALIDATION_REGEX);
     }
 
@@ -45,12 +45,12 @@ public class Phone {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof Phone)) {
+        if (!(other instanceof Priority)) {
             return false;
         }
 
-        Phone otherPhone = (Phone) other;
-        return value.equals(otherPhone.value);
+        Priority otherPriority = (Priority) other;
+        return value.equals(otherPriority.value);
     }
 
     @Override
