bc4e83b0e4536b6cf6ffd804740ba98dfb82e4f7
JonChng <jonathanchngyk@gmail.com>
2025-10-20 22:54:47 +0800

    Update parser

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -22,27 +22,29 @@ import seedu.address.model.attraction.Priority;
 import seedu.address.model.tag.Tag;
 
 /**
- * Parses input arguments and creates a new AddCommand object.
+ * Parses input arguments and creates a new AddCommand object
  */
 public class AddCommandParser implements Parser<AddCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the AddCommand and returns an AddCommand object
-     * for execution.
-     *
-     * @throws ParseException if the user input does not conform the expected format.
+     * Parses the given {@code String} of arguments in the context of the AddCommand
+     * and returns an AddCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
      */
     public AddCommand parse(String args) throws ParseException {
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PRIORITY,
-                PREFIX_CONTACT, PREFIX_ADDRESS, PREFIX_ACTIVITIES, PREFIX_TAG);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PRIORITY,
+                        PREFIX_CONTACT, PREFIX_ADDRESS, PREFIX_ACTIVITIES, PREFIX_TAG);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PRIORITY, PREFIX_CONTACT)) {
+            ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PRIORITY, PREFIX_CONTACT,
+                    PREFIX_ADDRESS, PREFIX_ACTIVITIES, PREFIX_TAG);
+        }
+
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS,
                 PREFIX_PRIORITY, PREFIX_CONTACT, PREFIX_ACTIVITIES)
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
-        }
-        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PRIORITY, PREFIX_CONTACT)) {
-            ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PRIORITY, PREFIX_CONTACT,
-                    PREFIX_ADDRESS, PREFIX_ACTIVITIES, PREFIX_TAG);
         }
 
         argMultimap.verifyNoDuplicatePrefixesFor(
@@ -60,8 +62,8 @@ public class AddCommandParser implements Parser<AddCommand> {
     }
 
     /**
-     * Returns true if none of the prefixes contains empty {@code Optional}
-     * values in the given {@code ArgumentMultimap}.
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
      */
     private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
         return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
